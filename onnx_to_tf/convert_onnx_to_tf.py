e04231f728f7bf12183d9f5e6da672da32627622
Rajashekar Chintalapati <rajashekar.chintalapati@walmart.com>
2021-09-07 10:36:03 -0700

    Fixing tflite version

--- onnx_to_tf/convert_onnx_to_tf.py
+++ onnx_to_tf/convert_onnx_to_tf.py
@@ -3,6 +3,8 @@ import onnx
 from onnx_tf.backend import prepare
 import tensorflow as tf
 
+import numpy as np
+
 
 onnx_model = onnx.load("onnx_model.onnx")  # load onnx model
 
@@ -20,7 +22,17 @@ print(tf_rep.tensor_dict)
 
 tf_rep.export_graph("hey_fourth_brain")  # export the model
 
+# Below didnt work, it was changing the input size
 # Converting a SavedModel.
-converter = tf.lite.TFLiteConverter.from_saved_model("hey_fourth_brain")
+# converter = tf.lite.TFLiteConverter.from_saved_model("hey_fourth_brain")
+# converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(
+#    "hey_fourth_brain/saved_model.pb", tf_rep.inputs, tf_rep.outputs, input_shapes={"input": [1, 1, 40, 61]}
+# )
+
+model = tf.saved_model.load("hey_fourth_brain")
+input_shape = [1, 1, 40, 61]
+func = tf.function(model).get_concrete_function(input=tf.TensorSpec(shape=input_shape, dtype=np.float32, name="input"))
+converter = tf.lite.TFLiteConverter.from_concrete_functions([func])
+
 tflite_model = converter.convert()
 open("hey_fourth_brain.tflite", "wb").write(tflite_model)
